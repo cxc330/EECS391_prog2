02c7408a9a35efd0d9f92bc247216ed1fee2697f
Christopher Gross <Chris@ChrisMac.local>
2012-02-26 19:09:15 -0500

    Changed the shell script. Can not seem to get the provided class files to run.. ours works though

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 javac -cp 'lib/SimpleRTSv3.1.jar' src/edu/cwru/SimpleRTS/agent/ABAgent.java
 
-java -cp lib/SimpleRTSv3.1.jar:src edu.cwru.SimpleRTS.Main --config data/proj1Config.xml data/game_2fv1a.map --agent  edu.cwru.SimpleRTS.agent.GameAgent 0 --agent edu.cwru.SimpleRTS.agent.visual.VisualAgent 0 --agentparam true --agentparam true
+java -cp lib/SimpleRTSv3.1.jar:src edu.cwru.SimpleRTS.Main --config data/proj1Config.xml data/game_2fv1a.map --agent  edu.cwru.SimpleRTS.agent.GameAgent 0 --agent edu.cwru.SimpleRTS.agent.ABAgent 1 --agent edu.cwru.SimpleRTS.agent.visual.VisualAgent 0 --agentparam true --agentparam true
